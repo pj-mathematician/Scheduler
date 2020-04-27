af7d2e1b5e7108bdaba2ba247939297926f7faf4
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2020-04-27 20:26:08 +0530

    Made the sign up page.
    It still needs to be linked to the login page

--- main.py
+++ main.py
@@ -3,23 +3,19 @@ from kivy.uix.screenmanager import Screen
 from kivymd.uix.label import MDLabel
 from kivymd.font_definitions import theme_font_styles
 from kivymd.app import MDApp
-
+from kivy.uix.screenmanager import *
 
 
 class Test(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.screen = Builder.load_file('login.kv')
+        self.screen = Builder.load_file('SignUp.kv')
 
     def build(self):
-        self.theme_cls.theme_style = "Dark"  
-
+        self.theme_cls.theme_style = "Dark"
         screen = Screen()
-    	
-
-
         return self.screen
 
-
-
-Test().run()
+if __name__ == '__main__':
+	Test().run()
+ 
