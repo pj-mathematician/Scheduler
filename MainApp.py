3218c1964c7145b45aaa472db79d0558e81cd1c2
Pj-Mathematician <55710610+pj-mathematician@users.noreply.github.com>
2020-10-11 18:37:27 +0530

    Breaking changes, made it front-end functional

--- MainApp.py
+++ MainApp.py
@@ -1,6 +1,10 @@
 import os
 import sys
+import time
+from operator import itemgetter
 from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.properties import StringProperty
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
@@ -8,14 +12,40 @@ from kivymd.app import MDApp
 from kivymd.uix.button import MDRaisedButton, MDFlatButton, MDFillRoundFlatIconButton
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.picker import MDTimePicker
+from kivymd.uix.card import MDCardSwipe
 if getattr(sys, "frozen", False):  
     os.environ["Scheduler-master"] = sys._MEIPASS
 else:
     sys.path.append(os.path.abspath(__file__).split("demos")[0])
     os.environ["Scheduler-master"] = os.path.dirname(os.path.abspath(__file__))
-
+def i2t(instance):
+	return instance.text2
 
 KV = '''
+<SwipeToDeleteItem>:
+    anchor:'right'
+    type_swipe: 'auto'
+    size_hint_y: None
+    height: content.height
+    on_swipe_complete:app.remove_item(root)
+
+    MDCardSwipeLayerBox:
+        padding: "8dp"
+
+        MDIconButton:
+            icon: "trash-can"
+            pos_hint: {"center_y": 0.5}
+            on_release: app.remove_item(root)
+
+    MDCardSwipeFrontBox:
+
+        TwoLineListItem:
+            id: content
+            text: root.text
+            secondary_text: root.text2
+            _no_ripple_effect: True
+
+
 <ContentNavigationDrawer>:
 
     ScrollView:
@@ -36,34 +66,37 @@ KV = '''
 
 
 Screen:
+	
 
-    MDToolbar:
-        id: toolbar
-        pos_hint: {"top": 1}
-        elevation: 10
-        title: "Scheduler"
-        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
 
     NavigationLayout:
-        x: toolbar.height
+        #x: toolbar.height
 
         ScreenManager:
             id: screen_manager
 
             Screen:
                 name: "todo"
-                MDFloatingActionButtonSpeedDial:
-                    data: app.data
-                	rotation_root_button: False
-                MDLabel:
-                    id: txt
-                    text: "MDDataTable"
-                    halign: "center"
-                MDFlatButton:
-                    text: "Text"
-                    id:but
-                    on_release:
-                        app.event_handler()
+                BoxLayout:
+                	orientation: "vertical"
+                	spacing: "10dp"
+				    MDToolbar:
+				        id: toolbar
+				        pos_hint: {"top": 1}
+				        elevation: 10
+				        title: "Scheduler"
+				        left_action_items: []
+				        right_action_items:[["plus", lambda x: app.add()],["refresh", lambda x: app.refresh_callback()]]                
+	
+                
+
+	                ScrollView:
+	          
+	                	MDList:
+	                		id:md_list
+	                		padding: "2dp"
+	                	
+
 
             Screen:
                 name: "edit"
@@ -71,7 +104,11 @@ Screen:
                 MDLabel:
                     text: "edit here"
                     halign: "center"
-
+                MDFloatingActionButtonSpeedDial:
+                    data: app.data
+                   # on_release:
+                   #	app.event_handler()
+                	rotation_root_button: False
         MDNavigationDrawer:
             id: nav_drawer
 
@@ -81,7 +118,10 @@ Screen:
 '''
 class custom_events(BoxLayout):
    pass
-    
+   
+class SwipeToDeleteItem(MDCardSwipe):
+    text = StringProperty()
+    text2 = StringProperty()
 
 
 class ContentNavigationDrawer(BoxLayout):
@@ -90,6 +130,8 @@ class ContentNavigationDrawer(BoxLayout):
 
 
 class TestNavigationDrawer(MDApp):
+    temp_instance=None
+    main_event_list=[]
     my_event=None
     mytime=None
     data = {'delete':'Delete','pencil':'Add'}
@@ -101,27 +143,28 @@ class TestNavigationDrawer(MDApp):
     def build(self):
         self.theme_cls.theme_style = 'Dark'
         Builder.load_file(f"{os.environ['Scheduler-master']}/KivyFiles/Main App/content.kv")
+        
         return Builder.load_string(KV)
+        self.refresh_callback()
+        
 
     cstm_evnt = None
+    dlt_event= None
 
-    def event_handler(self):
+
+    def add(self):
         #self.time_dialog=MDTimePicker()
         if not self.cstm_evnt:
             self.cstm_evnt = MDDialog(
                                     
-                                    title='[color=#FFFFFF]Custom Event[/color]',
+                                    title='[color=#FFFFFF]Add Event[/color]',
                                     type='custom',
                                     content_cls=custom_events(),
                                     size_hint=(0.4, 0.3),
                                     buttons=[MDFlatButton(text='CANCEL',
                                                           on_release=self.cstm_evnt_close,
                                                           text_color=self.theme_cls.primary_color),
-
-                                    MDRaisedButton(text='OK',
-                                                           on_release=self.cstm_evnt_append,
-                                                           text_color=self.theme_cls.primary_color,),
-                                    MDFillRoundFlatIconButton(text='Set',
+                                    MDFillRoundFlatIconButton(text='Set Time',
                                                            icon='clock',
                                                            on_release=self.time_picker
                                                            )
@@ -130,21 +173,67 @@ class TestNavigationDrawer(MDApp):
         self.cstm_evnt.open()
     def time_picker(self, *args):        
         self.time_dialog=MDTimePicker()
+        #self.time_dialog.title=self.my_event
         self.time_dialog.bind(time=self.show)
-        self.time_dialog.open()        
+        self.time_dialog.open()
+        self.cstm_evnt.dismiss()
+        self.cstm_event=None
+    #confirmd=None
+    
+    def confirm(self):
+    	if not self.confirmd:
+    		self.confirmd=MDDialog(title='[color=#FFFFFF]Confirm[/color]',size_hint=(0.4, 0.3),text="Add Event {} at {} ?".format(self.my_event,self.mytime),buttons=[MDFlatButton(text='NO',
+                                                          on_release=self.clconfirmd,
+                                                          text_color=self.theme_cls.primary_color),
+                                    MDRaisedButton(text='YES',
+                                                           on_release=self.event_adder
+                                                           )
+                                                ])
+    		self.confirmd.open()
+    def clconfirmd(self,*args):
+    	self.confirmd.dismiss()
+    	
+    def refresh_callback(self, *args):
+        self.main_event_list=sorted(self.main_event_list,key=itemgetter(1))
+        self.root.ids.md_list.clear_widgets()
+        for i in self.main_event_list:
+        	self.root.ids.md_list.add_widget(SwipeToDeleteItem(text=i[0],text2=i[1]))
+        	#self.root.ids.md_list.add_widget(i)
+    def textfrominstance(self,instance):
+        	return instance.text
+        	
     def show(self,instance,time):
         self.mytime=time
+        self.confirmd=None
+        self.confirm()
         #fixedtimedict[key]=time
     def cstm_evnt_close(self,*args):
         self.cstm_evnt.dismiss()
-    def cstm_evnt_append(self, *args):
-        cont=custom_events()
-        my_event=(cont.ids.evnt.text)
-        custom_events().ids.evnt.text=None
-        self.cstm_evnt.dismiss()
-        self.root.ids.txt.text=self.my_event + " " + str(self.mytime)
-        print(self.root.ids.txt.text)
         
-
-if __name__ == "__main__":        
+    def remove_item(self,instance):
+        print(instance.text)
+        self.temp_instance=instance
+        if not self.dlt_event:
+    	    self.dlt_event=MDDialog(title='[color=#FFFFFF]Confirm Delete[/color]',size_hint=(0.4, 0.3),text="Delete {}?".format(instance.text),buttons=[MDFlatButton(text='NO',on_release=self.cldelevent,text_color=self.theme_cls.primary_color),MDRaisedButton(text='YES',on_press=self.rem)])
+    	    self.dlt_event.open()
+    def rem(self,instance):
+    	print(self.main_event_list)
+    	self.dlt_event.dismiss()
+    	self.root.ids.md_list.remove_widget(self.temp_instance)
+    	for i in range(len(self.main_event_list)):
+    		if self.main_event_list[i][0]==self.temp_instance.text:
+    			del(self.main_event_list[i])
+    			print(self.main_event_list)
+    			break
+    	self.dlt_event=None
+    	
+    def cldelevent(self,instance):
+    	self.dlt_event.dismiss()
+    def event_adder(self,instance):
+    	self.confirmd.dismiss()
+    	
+    	instance=SwipeToDeleteItem(text=self.my_event,text2=str(self.mytime))
+    	self.main_event_list.append((instance.text,instance.text2))
+    	self.root.ids.md_list.add_widget(instance)
+if __name__ == "__main__":         
     TestNavigationDrawer().run()
