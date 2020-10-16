e3fb62a46483a3202acf44f3121875790fe7ec5b
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2020-10-16 20:18:55 +0530

    Added Logic.py, made changes to mysqllog.py
    Changed server type back to offline due to some issues we couldn't resolve with the online one.

---
+++ Logic.py
@@ -0,0 +1,205 @@
+#i/p
+from math import floor, ceil
+
+info = {
+    'wakeup': 480,
+    'breakfast': 540,
+    'lunch': 840,
+    'meeting': (900, 1020),
+    'dinner': 1200,
+    'sleep': 1440
+}
+
+things_td = {'maths': 240, 'physics': 180, 'chem': 180}
+
+prod_time = 0
+for i in list(things_td.values()):
+    prod_time+=i
+
+# Needs to be calculated from info
+"""
+NOTE : All times are in minutes in a day
+buffer length = 30 min
+• Insert buffer length between two events
+• If time b/w two events <= 30, then count it in leisure
+• Adjust remaining time or required time in the sleep time
+• Assume all standard events take 30 min, so pretty much add buffer to them
+"""
+"""
+x
+• if x<120, prod_time = floor(120 * 0.75)
+• if x>120
+    1) x%60<=30, make 2 hr and rest is leisure
+    2) If 1) is false, apply scaling
+NOTE : 2hr slot is 1:30 minutes of work and 30 minute leisure
+"""
+
+
+def ft_calc(d):
+    var = 0
+
+    time_slots = list(d.values())
+    time_slots.sort
+    for j, i in enumerate(time_slots):
+        if j == len(time_slots) - 1:
+            continue
+
+        elif type(i) != type(tuple()):
+            i = tuple([i, i + 30])
+            time_slots[j] = i
+
+    return var, time_slots
+
+
+# def sleep_modif():
+#     global free_time
+#     if (prod_time - free_time) < 0:
+#         info['sleep'] += (prod_time - free_time)
+#         return True
+
+
+def fts_calc(ts):
+    free_time_slots = []
+
+    for i in range(len(ts) - 2):
+        temp = (ts[i][1], ts[i + 1][0])
+        free_time_slots.append(temp)
+
+    free_time_slots.append((ts[-2][1], ts[-1]))
+    return free_time_slots
+
+
+def slot_decision(w):
+    p = []
+    f = []
+    rval = []
+    if w < 120:
+        p.append(90)
+        f.append(w - 90)
+    else:
+        for i in range(w // 120):
+            p.append(90)
+            f.append(30)
+
+        temp = w - ((i + 1) * 120)
+        if temp <= 30:
+            f.append(temp)
+        else:
+            p.append(floor(temp * 0.75))
+            f.append(ceil(temp * 0.25))
+
+    for i in range(len(p)):
+        rval.append(p[i])
+        rval.append(f[i])
+
+    if f[-1] != 0:
+        rval.append(f[-1])
+
+    return rval
+
+
+def assign(tasks, scheds):
+    
+    """
+    Assigns every time slot a task to perform.
+    """
+    task = list(tasks.keys())
+    sched = scheds
+    l = []
+
+    for j, i in enumerate(sched):
+        
+        k = j % len(tasks)
+        if tasks[task[k]]==0:
+            for p in range(len(tasks)):
+                if tasks[task[p]] != 0:
+                    k = p
+                    break
+            else:
+                break
+            
+
+        if tasks[task[k]] > 0:
+            if tasks[task[k]] >= (i[1] - i[0]):
+                l.append((i[0], i[1], task[k]))
+                tasks[task[k]] -= (i[1] - i[0])
+
+            elif tasks[task[k]] < (i[1] - i[0]):
+                l.append((i[0], i[0] + tasks[task[k]], task[k]))
+
+                temp = i[0] + tasks[task[k]]
+                tasks[task[k]] = 0
+
+                for m in range(len(tasks)):
+                    if tasks[task[m]] >= (i[1] - temp) and tasks[task[m]] !=0 :
+                        l.append((temp, i[1], task[m]))
+                        tasks[task[m]]-=(i[1] - temp)
+                    elif tasks[task[m]] < (i[1] - temp) and tasks[task[m]] !=0 :
+                        l.append((temp, temp+tasks[task[m]] , task[m]))
+                        temp+=tasks[task[m]]
+                        tasks[task[m]] =0          
+        else:
+            continue
+
+    l.sort()
+    return l
+
+
+def schedule(fts):
+    leisure = []
+    sched = []
+    prod_time = 0
+    faltu = 0
+
+    tracker = fts[0][0]
+    for i in fts:
+        tracker = i[0]
+        diff = i[1] - i[0]
+
+        if diff <= 30:
+            sched.append(i)
+            tracker += diff
+        else:
+            temp = slot_decision(diff)
+            for j in range(0, len(temp) - 1, 2):
+                sched.append((tracker, tracker + temp[j]))
+                tracker += temp[j]
+                leisure.append((tracker, tracker + temp[j + 1]))
+                tracker += temp[j + 1]
+
+            if tracker != i[1]:
+                sched.append((tracker, i[1]))
+
+    for i in sched:
+        prod_time += i[1] - i[0]
+    for i in leisure:
+        faltu += i[1] - i[0]
+
+    return sched, leisure, prod_time, faltu
+
+
+# -- main -- #
+def return_sched(info, things_td):
+    prod_time = 0
+    time_slots = ft_calc(info)[1]
+    free_time = schedule(fts_calc(time_slots))[2]
+    sched = schedule(fts_calc(time_slots))[0]
+    leisure = schedule(fts_calc(time_slots))[1]
+
+    for i in list(things_td.values()):
+        prod_time+=i
+
+    if prod_time > free_time:
+        temp = leisure.pop(-1)
+        sched.append(temp)
+        free_time += (temp[1] - temp[0])
+        if prod_time > free_time:
+            a, b = sched[-1]
+            b += prod_time - free_time
+            sched[-1] = a, b
+            free_time = prod_time
+            sched.append((sched[-2][0], sched[-1][1]))
+            del sched[-2:-4:-1]
+
+    return (assign(things_td, sched))
+
--- mysqllog.py
+++ mysqllog.py
@@ -1,7 +1,7 @@
 import localsql
+from Logic import return_sched
 
 cur = localsql.mycon.cursor()
-
 
 def check_db():
     """
@@ -9,6 +9,7 @@ def check_db():
     Done so that user doesn't have to do more than just make the database and adjust localsql
 
     EDIT : We switched to an online MySQL server, so this is redundant at this point, but it will stay here anyway
+    EDIT #2 : Due to some complications, we have switched back to the offline database(OOF)
     """
     cur.excecute("SHOW TABLES")
     table_list = cur.fetchall()
@@ -16,22 +17,31 @@ def check_db():
     if ("usepass",) not in table_list:
         query = "CREATE TABLE usepass(EMAIL VARCHAR(254) NOT NULL PRIMARY KEY, PASS VARCHAR(40) NOT NULL)"
         cur.execute(query)
-
+        localsql.mycon.commit()
 
     if ("things_td",) not in table_list:
         query = "CREATE TABLE user_data(EMAIL VARCHAR(254) NOT NULL , SUBJECT VARCHAR(40) NOT NULL, TIME INT NOT NULL)"
         cur.execute(query)
+        localsql.mycon.commit()
+
+    if ("routine",) not in table_list:
+        query = "CREATE TABLE routine(EMAIL VARCHAR(254) NOT NULL , TASK VARCHAR(40) NOT NULL, START_TIME INT NOT NULL, END_TIME INT NOT NULL)"
+        cur.execute(query)
+        localsql.mycon.commit()
 
 
 def check(email,pwd):
-    ##Returns 0 if incorrect username, 1 if everything is correct :-D, 2 if password is incorrect :-O##
+    """Returns 0 if incorrect username :-
+    0 if username incorrect :-O
+    1 if everything is correct :-D
+    2 if password is incorrect :-O """
     
     cur.execute('select exists(select * from usepass where email="%s")'%email)    #check for existance of a username
     
-    rese=cur.fetchone()    #tuple containing a boolean value.For example--(1,) 
+    rese = cur.fetchone()    #tuple containing a boolean value.For example--(1,) 
     if rese[0]:    #if 1
         cur.execute('select pass from usepass where email="%s"'%email)    #get the corresponding true password
-        resp=cur.fetchone()    #tuple containing password.For example--('pass123',)
+        resp = cur.fetchone()    #tuple containing password.For example--('pass123',)
 
         if pwd==resp[0]:    # if input password == true password
             return 1    # PASSWORD CORRECT
@@ -64,7 +74,48 @@ def add_user_data(email, items):
     """
     Function to add the subjects and the time assigned to such tasks
     NOTE : items is a list of tuples containing records in the format => (<subject>, <time>)
+           Time should be in the standard HH:MM:SS format
     """
     query = "INSERT INTO user_data VALUES('{}', %s, %s)".format(email)
     cur.executemany(query, items)
-    mycon.commit()
+    localsql.mycon.commit()
+
+
+def add_routine(email, tasks):
+    """
+    Function to add the daily routine into table "routine"
+    NOTE : tasks is a list of tuples containing records in the format => (<event>, <time>)
+           Time should be in the standard HH:MM:SS format
+    """
+    query = "INSERT INTO routine VALUES('{}', %s, %s)".format(email)
+    cur.executemany(query, items)
+    localsql.mycon.commit()
+
+
+def return_schedule(email):
+    """
+    Function to retrieve the user data after succesful login, and give the corresponding schedule back to the user
+    Based on the assumption that the email exists in the database
+    """
+
+    query = f"SELECT SUBJECT, TIME FROM user_data WHERE EMAIL = {email}"
+    cur.execute(query)
+    val = cur.fetchall()
+
+    things_td = {}
+    for i in val:
+        temp = int(i[1])
+        time = 60 * int(temp[:2:]) + int(temp[3:5:]) # Converting time to minutes
+        things_td[i[0]] = time
+
+    query = f"SELECT TASK, TIME FROM routine WHERE EMAIL = {email}"
+    cur.execute(query)
+    val = cur.fetchall()
+
+    info = {}
+    for i in val:
+        temp = int(i[1])
+        time = 60 * int(temp[:2:]) + int(temp[3:5:]) # Converting time to minutes
+        info[i[0]] = time
+
+    return return_sched(things_td, info)
