a5f9eabfa4efa6a22e530a9b58a240a59fe5b962
Anirudh S. Kumar <61222180+Anirudh-S-Kumar@users.noreply.github.com>
2022-01-31 22:11:30 +0530

    Update localsql.py

--- localsql.py
+++ localsql.py
@@ -1,7 +1,7 @@
 import mysql.connector as connector
 
 #mycon = connector.connect(host='db4free.net', user='mdscheduler', passwd='password', database='mdscheduler',port='3306')
-mycon = connector.connect(host='localhost', user='root', passwd='10717', database='scheduler',port='3306')
+mycon = connector.connect(host='localhost', user='root', passwd='*****', database='scheduler',port='3306')
 
 if mycon.is_connected():
 	print('Connected')
